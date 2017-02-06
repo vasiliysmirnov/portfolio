3770ec54c4664f956ac4452450cf7ae21522d7e2
Mac <mac@iMac-iMaster.local>
2017-02-06 18:03:21 +0300

    logo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template lang='jade'>
   .app#app
-    img(src='./assets/images/logo.png')
     hello
+    header-block
 </template>
 
 <script>
 import Hello from './components/Hello/index.vue'
+import HeaderBlock from './components/Modules/Header/index.vue'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Hello,
+    HeaderBlock
   }
 }
 </script>
 
 <style lang='stylus' scoped>
+  @require './assets/styles/reset.styl'
+
   .app
     box-sizing border-box
     & *
       box-sizing border-box
+ 
 </style>
---
+++ src/components/Modules/Header/index.vue
@@ -0,0 +1,6 @@
+<template lang='jade'>
+  header.logo
+    a.narubalie(href='*', target='_blank') Narubalie
+</template>
+<script type="text/babel" src='./index.js'></script>
+<style lang='stylus' scoped src='./index.styl'></style>
